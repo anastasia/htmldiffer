d2348079e94807ad72e4c80d2e83f3dda44587b2
anastasia <anastasia.aizman@gmail.com>
2017-03-11 14:55:18 -0500

    add - functionality

--- diff.py
+++ diff.py
@@ -8,10 +8,23 @@ __version__ = '0.22'
 
 import difflib, string, re
 
-def isTag(x):
+def diff_tag(diff_type, text):
+    if is_tag(text):
+        # print "is tag:", text
+        return "<span class=diff_%s>%s</span>" % (diff_type, text)
+    else:
+        return "<span class=diff_%s>%s</span>" % (diff_type, text)
+
+def is_tag(x):
     return x[0] == "<" and x[-1] == ">"
 
-def textDiff(a, b):
+def is_text(x):
+    return ("<" and ">") not in x
+
+def is_div(x):
+    return x[0:4] == "<div" and x[-6:] == "</div>"
+
+def text_diff(a, b):
     """Takes in strings a and b and returns a human-readable HTML diff."""
 
     out = []
@@ -21,31 +34,15 @@ def textDiff(a, b):
     except TypeError:
         s = difflib.SequenceMatcher(None, a, b)
     for e in s.get_opcodes():
-        if e[0] == "replace":
-            # @@ need to do something more complicated here
-            # call textDiff but not for html, but for some html... ugh
-            # gonna cop-out for now
-            """ ignoring all tags """
-            idx = e[3]
-            while idx <= e[4]:
-                just_text = ''
-                if not isTag(b[idx]):
-                    just_text += b[idx]
-                else:
-                    if len(just_text):
-                        out.append('<span class="diff_insert">'+''.join(just_text)+'</span>')
-                        just_text = ''
-                        out.append(''.join(b[idx]))
-                if len(just_text):
-                    out.append('<span class="diff_insert">'+''.join(just_text)+'</span>')
-                idx += 1
-
+        import ipdb; ipdb.set_trace()
+        old_el = a[e[1]:e[2]]
+        new_el = b[e[3]:e[4]]
+        if e[0] == "replace" or e[0] == "insert":
+            out.append(wrap_text("insert", new_el))
         elif e[0] == "delete":
-            out.append('<span class="diff_delete">'+ ''.join(a[e[1]:e[2]]) + "</span>")
-        elif e[0] == "insert":
-            out.append('<span class="diff_insert">'+''.join(b[e[3]:e[4]]) + "</span>")
+            out.append(wrap_text("delete", old_el))
         elif e[0] == "equal":
-            out.append(''.join(b[e[3]:e[4]]))
+            out.append(''.join(new_el))
         else:
             raise "Um, something's broken. I didn't expect a '" + `e[0]` + "'."
     out.append("""<style>
@@ -60,6 +57,32 @@ def textDiff(a, b):
     </style>""")
     return ''.join(out)
 
+def is_whitelisted_tag(x):
+    whitelisted_tags = ["<img", "<input"]
+    for tag in whitelisted_tags:
+        if tag in x:
+            return True
+    return False
+
+def wrap_text(diff_type, text_list):
+    idx, just_text, outcome = [0, '', []]
+    while idx < len(text_list):
+        el = text_list[idx]
+        if is_tag(el):
+            if len(just_text):
+                outcome.append(diff_tag(diff_type, just_text))
+                just_text = ''
+            if is_whitelisted_tag(el):
+                outcome.append(diff_tag(diff_type, just_text))
+            else:
+                outcome.append(el)
+        else:
+            just_text += el
+        idx += 1
+    return ''.join(outcome)
+
+
+
 def html2list(x, b=0):
     rx = re.compile('\n|\t|\r|\s{2}')
     mode = 'char'
@@ -70,18 +93,25 @@ def html2list(x, b=0):
     for c in x:
         if mode == 'tag':
             if c == '>':
-                if b: cur += ']'
-                else: cur += c
+                if b:
+                    cur += ']'
+                else:
+                    cur += c
                 out.append(cur); cur = ''; mode = 'char'
-            else: cur += c
+            else:
+                cur += c
         elif mode == 'char':
             if c == '<':
                 out.append(cur)
-                if b: cur = '['
-                else: cur = c
+                if b:
+                    cur = '['
+                else:
+                    cur = c
                 mode = 'tag'
-            elif c in string.whitespace: out.append(cur+c); cur = ''
-            else: cur += c
+            elif c in string.whitespace:
+                out.append(cur+c); cur = ''
+            else:
+                cur += c
     out.append(cur)
     return filter(lambda x: x is not '', out)
 
@@ -93,4 +123,4 @@ if __name__ == '__main__':
         print "htmldiff: highlight the differences between two html files"
         print "usage: " + sys.argv[0] + " a b"
         sys.exit(1)
-    print textDiff(open(a).read(), open(b).read())
+    print text_diff(open(a).read(), open(b).read())
