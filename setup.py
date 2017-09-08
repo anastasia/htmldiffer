6845bfd13a0f317dabbee67488a0ca77729a0ace
anastasia <anastasia.aizman@gmail.com>
2017-09-08 00:06:21 -0400

    bump - setup

--- setup.py
+++ setup.py
@@ -1,35 +1,20 @@
-import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
-    name="htmldiff",
-    version="0.23",
-    author="Aaron Swartz",
+    name="htmldiffer",
+    version="0.24",
+    author="Anastasia Aizman",
     author_email="anastasia.aizman@gmail.com",
     description=("HTML diff"),
     license="BSD",
     keywords="html diff",
-    url="http://www.aaronsw.com/2002/diff",
+    url="https://github.com/anastasia/htmldiff",
     packages=['htmldiff', 'tests'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
     ],
+    python_requires='>=2.7'
 )
 
-
-#
-# """HTML Diff: http://www.aaronsw.com/2002/diff
-# Rough code, badly documented. Send me comments and patches."""
-#
-# __author__ = 'Aaron Swartz <me@aaronsw.com>'
-# __copyright__ = '(C) 2003 Aaron Swartz. GNU GPL 2 or 3.'
-# __version__ = '0.22'
